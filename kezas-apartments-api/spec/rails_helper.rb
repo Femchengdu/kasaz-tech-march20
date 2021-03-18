3cb5d2dc30b3e637cfc580e4233a3d7e6294e644
范淼 <femiajayi705@gmail.com>
2021-03-18 16:21:13 +0800

    chore: Added configs for helper

--- kezas-apartments-api/spec/rails_helper.rb
+++ kezas-apartments-api/spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
-
+  config.include RequestSpecHelper, type: :request
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
