3016ad67afabb506b91f3753b0df5b796e364e70
范淼 <femiajayi705@gmail.com>
2021-03-20 06:11:02 +0800

    feat: Added loading and error state

--- kezas-apartments-frontend/src/hooks/useGetApartments.js
+++ kezas-apartments-frontend/src/hooks/useGetApartments.js
@@ -3,15 +3,24 @@ import { fetchApartments } from "../utils/fetchApartments";
 
 const useGetApartments = () => {
   const [apartments, setApartments] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     (async () => {
-      const { data } = await fetchApartments();
-      if (data) {
-        setApartments(data);
+      setLoading(true);
+      try {
+        const { data } = await fetchApartments();
+        if (data) {
+          setApartments(data);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log("Fetch error :", err.message);
+        setError(true);
       }
     })();
   }, []);
-  return apartments;
+  return [apartments, loading];
 };
 
 export default useGetApartments;
